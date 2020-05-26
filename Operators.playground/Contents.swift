7d2041233307629888f7be68d17b853c3f1f439f
William Skrzypczak <64371704+MobilePathwaysPartners@users.noreply.github.com>
2020-05-26 13:02:53 -0500

    Commit

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,43 @@
+
+/*: - Copyright :  Bulldog Ventures Inc  ©  2020 */
+import UIKit
+/*: ### Operators
+ An operator is a special symbol or phrase that you use to check, change, or combine values.
+ Operators are unary, binary, or ternary
+ - Unary operators operate on a single target
+ - Binary operators operate on two targets
+ - Ternary operators operate on three targets
+ #### Examples*/
+/*:
+ * Callout(=):
+ doesnt return a value, it *assigns* a value from one data type to another.
+*/
+var myVar = 3
+var myNewVar = myVar
+/*:
+ * Callout(+,-,*,/):
+ are considered arithmetic operators, they perform the arithmetic tasks that you would expect.
+*/
+var a = 2
+var b = 2
+
+var c = a+b
+var d = a-b
+var e = a*b
+var f = a/b
+/*:
+ * Callout(==,!=,>,<,>=,<=):
+ are considered comparison operators.
+ - Each of the comparison operators returns a Bool value to indicate whether or not the statement is true
+*/
+1 == 1   // true because 1 is equal to 1
+2 != 1   // true because 2 is not equal to 1
+2 > 1    // true because 2 is greater than 1
+1 < 2    // true because 1 is less than 2
+1 >= 1   // true because 1 is greater than or equal to 1
+2 <= 1   // false because 2 is not less than or equal to 1
+
+ 
+ 
+ 
+
